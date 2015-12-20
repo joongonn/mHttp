ef8e3733d1f24c70f5c0bb370e1883c5585088b8
joongonn <joongonn@gmail.com>
2015-12-19 22:27:23 -0800

    factor non-http codes into contrib project

---
+++ mHttp.Contrib/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("mHttp.Contrib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("mHttp.Contrib")]
+[assembly: AssemblyCopyright("joongonn")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.1.*")]
+
+[assembly: InternalsVisibleTo("mHttp.Tests")]
